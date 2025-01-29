0a18f1a217b13735dfed4c126d2661619f8975ba
margar <margar-melkonyan>
2025-01-29 21:24:42 +0300

    start working on user repository

--- internal/repository/user.go
+++ internal/repository/user.go
@@ -1,1 +1,37 @@
 package repository
+
+import (
+	"database/sql"
+	"time"
+)
+
+type User struct {
+	ID           uint64
+	Nickname     string
+	Firstname    string
+	Lastname     string
+	Patronymic   string
+	Email        string
+	Password     string
+	RefreshToken string
+	CreatedAt    time.Time
+	UpdatedAt    time.Time
+	DeletedAt    *time.Time
+}
+
+type UserRepositoryInterface interface {
+	Get(id uint64) (*User, error)
+	GetByNickname(nickname string) (*User, error)
+	GetByEmail(email string) (*User, error)
+	Create(user *User) error
+	Update(user *User, id uint64) error
+	Delete(id uint64) error
+}
+
+type UserRepository struct {
+	db *sql.DB
+}
+
+func NewUserRepository(db *sql.DB) *UserRepository {
+	return &UserRepository{db: db}
+}
--- internal/router/auth.go
+++ internal/router/auth.go
@@ -7,9 +7,11 @@ import (
 func authRoutes() *http.ServeMux {
 	auth := http.NewServeMux()
 
-	auth.HandleFunc("GET /current-user", func(w http.ResponseWriter, r *http.Request) {})
 	auth.HandleFunc("POST /sign-in", func(w http.ResponseWriter, r *http.Request) {})
 	auth.HandleFunc("POST /sign-up", func(w http.ResponseWriter, r *http.Request) {})
+	auth.HandleFunc("GET /current-user", func(w http.ResponseWriter, r *http.Request) {})
+	auth.HandleFunc("GET /sign-out", func(w http.ResponseWriter, r *http.Request) {})
+	auth.HandleFunc("POST /refresh-token", func(w http.ResponseWriter, r *http.Request) {})
 
 	return auth
 }
