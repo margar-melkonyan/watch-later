fd3b49b98175d003fdbfe8490e9ae1a485f91706
margar <margar-melkonyan>
2025-03-25 22:39:56 +0300

    refactor: start refactoring validation

---
+++ internal/lang/eng/validation.go
@@ -0,0 +1,14 @@
+package eng
+
+var messages = map[string]string{
+	"required": "Поле {field} обязательно для заполнения.",
+	"email":    "Поле {field} должно быть корректным адресом электронной почты.",
+	"min":      "Поле {field} должно содержать не менее {param} символов.",
+	"max":      "Поле {field} должно содержать не более {param} символов.",
+	"gte":      "Поле {field} должно быть больше или равно {param}.",
+	"lte":      "Поле {field} должно быть меньше или равно {param}.",
+}
+
+func GetMessages() map[string]string {
+	return messages
+}
