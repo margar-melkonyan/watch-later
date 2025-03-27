56846671ad60e3484d7f549ea7547ba146b57c24
Mark M <>
2025-03-27 21:48:40 +0300

    feat: start adding auth sigin method

---
+++ internal/common/auth.go
@@ -0,0 +1,9 @@
+package common
+
+type SignInUser struct {
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,min=8,max=32"`
+}
+
+type ResponseUser struct {
+}
